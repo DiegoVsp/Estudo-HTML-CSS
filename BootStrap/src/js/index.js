4532f5f7cd9050cba08e393db0b9a86ff11e4e7e
Diego Vespa <diego-vespa2020@outlook.com.br>
2020-08-25 14:15:42 -0300

    finalizado

---
+++ BootStrap/src/js/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap';
+import '../css/styles.scss';
+import '../assets/harley.jpg'
+
+const elModalOpen = document.querySelector('#modal-open');
+elModalOpen.addEventListener('click', () =>{
+   $('#my-modal').modal('show');
+});
+
+const elModalClose = document.querySelector('#modal-close');
+elModalClose.addEventListener('click', () =>{
+  $('#my-modal').modal('hide');
+});
