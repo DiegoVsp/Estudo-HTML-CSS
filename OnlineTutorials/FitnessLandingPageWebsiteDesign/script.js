d8f8fe647cd2b2510c58aeab848278e2190ada6f
Diego Vespa <diego-vespa2020@outlook.com.br>
2020-09-13 17:23:10 -0300

    finalizando página

---
+++ OnlineTutorials/FitnessLandingPageWebsiteDesign/script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const nav = document.querySelector('.navigation')
+  const toggle = document.querySelector('.toggleMenu')
+
+  toggle.classList.toggle('ativo')
+  nav.classList.toggle('ativo')
+}
+
+
+
